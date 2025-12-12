71d8d237045a762bf4233e318f57779bc870a399
Konstantsin <k4164724@gmail.com>
2025-12-12 17:58:18 +0300

    commit 13

--- src/setup-app.ts
+++ src/setup-app.ts
@@ -4,7 +4,7 @@ import { testingRouter } from './testing/routers/testing.router';
 import { BLOGS_PATH, POSTS_PATH, TESTING_PATH } from './core/paths/paths';
 import { postsRoute } from './posts/routes/postsRoute';
 
-export const setupApp = async (app: Express) => {
+export const setupApp = (app: Express) => {
   app.use(express.json());
 
   app.use(BLOGS_PATH, blogsRouter);
