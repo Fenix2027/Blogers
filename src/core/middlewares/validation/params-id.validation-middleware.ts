6f289d2aa89f716652fae9d2ad3d1b80ed5ff7d2
Konstantsin <k4164724@gmail.com>
2025-12-12 18:15:51 +0300

    commit 14

--- src/core/middlewares/validation/params-id.validation-middleware.ts
+++ src/core/middlewares/validation/params-id.validation-middleware.ts
@@ -1,9 +1,19 @@
-import { param } from 'express-validator';
+import { body, param } from 'express-validator';
 
 export const idValidation = param('id')
   .exists()
   .withMessage('ID is required') // Проверка на наличие
   .isString()
   .withMessage('ID must be a string') // Проверка, что это строка
-  .isLength({ min: 1 })
-  .withMessage('ID must not be empty'); // Проверка, что строка не пустая
+  .isMongoId()
+  .withMessage('Неверный формат ObjectId');
+
+export const dataIdMatchValidation = body('data.id')
+  .exists()
+  .withMessage('ID in body is required')
+  .custom((value, { req }) => {
+    if (value !== req?.params?.id) {
+      throw new Error('ID in URL and body must match');
+    }
+    return true; // Если все хорошо, возвращаем true
+  });
